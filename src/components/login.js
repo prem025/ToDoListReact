a6ede8d95e3ed43a6549a90bcec15383689f4bdd
prem chandra <prem.chandra@playerzpot.com>
2022-09-11 08:25:11 +0530

    changes

--- src/components/login.js
+++ src/components/login.js
@@ -14,25 +14,37 @@ export default function Login() {
     }
 
     return(
-        <div className="row justify-content-center pt-5">
-            <div className="col-sm-6">
-                <div className="card p-4">
-                    <h1 className="text-center mb-3">Login </h1>
-                    <div className="form-group">
-                        <label>Email address:</label>
-                        <input type="email" className="form-control" placeholder="Enter email"
-                            onChange={e=>setEmail(e.target.value)}
-                        id="email" />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Password:</label>
-                        <input type="password" className="form-control" placeholder="Enter password"
-                            onChange={e => setPassword(e.target.value)}
-                        id="pwd" />
-                    </div>
-                    <button type="button" onClick={submitForm} className="btn btn-primary mt-4">Login</button>
+        <>
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-md space-y-8">
+              <div>
+                
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Login</h2>
+                
+              </div>
+             
+              
+                <div className="-space-y-px rounded-md shadow-sm">
+                  <div>
+                    <label className="sr-only">Email address</label>
+                    <input id="email" onChange={e=>setEmail(e.target.value)} type="email"  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 my-2 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"/>
+                  </div>
+                  <div>
+                    <label className="sr-only">Password</label>
+                    <input id="pwd" onChange={e => setPassword(e.target.value)} type="password"  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password"/>
+                  </div>
                 </div>
+                
+                 <div>
+                  <button type="button" onClick={submitForm} className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    
+                    Login
+                  </button>
+                </div>
+             
             </div>
-        </div>
+          </div>
+        </>
+    
     )
 }
--- src/components/register.js
+++ src/components/register.js
@@ -17,32 +17,42 @@ export default function Register() {
     }
 
     return(
-        <div className="row justify-content-center pt-5">
-            <div className="col-sm-6">
-                <div className="card p-4">
-                    <h1 className="text-center mb-3">Register </h1>
-                    <div className="form-group">
-                        <label>Name:</label>
-                        <input type="test" className="form-control" placeholder="Enter name"
-                            onChange={e=>setName(e.target.value)}
-                        id="email" />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Email address:</label>
-                        <input type="email" className="form-control" placeholder="Enter email"
-                            onChange={e=>setEmail(e.target.value)}
-                        id="email" />
-                    </div>
 
-                    <div className="form-group mt-3">
-                        <label>Password:</label>
-                        <input type="password" className="form-control" placeholder="Enter password"
-                            onChange={e => setPassword(e.target.value)}
-                        id="pwd" />
-                    </div>
-                    <button type="button" onClick={submitForm} className="btn btn-primary mt-4">Register</button>
+        <>
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-md space-y-8">
+              <div>
+                {/* <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register</h2>
+                
+              </div>
+            
+                <input type="hidden" name="remember" value="true"/>
+                <div className="-space-y-px rounded-md shadow-sm">
+                  <div>
+                    <label for="full_name" className="sr-only">Full Name</label>
+                    <input id="full_name" onChange={e=>setName(e.target.value)}  name="email" type="text" autocomplete="off" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 my-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Full Name"/>
+                  </div>
+                  <div>
+                    <label for="email-address"  className="sr-only">Email address</label>
+                    <input id="email-address" onChange={e=>setEmail(e.target.value)} name="email" type="email" autocomplete="email" required className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 my-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"/>
+                  </div>
+                  <div>
+                    <label for="password" className="sr-only">Password</label>
+                    <input id="password" onChange={e => setPassword(e.target.value)} name="password" type="password" autocomplete="current-password" required className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password"/>
+                  </div>
                 </div>
+                
+                 <div>
+                  <button type="button" onClick={submitForm} className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    
+                    Register
+                  </button>
+                </div>
+             
             </div>
-        </div>
+          </div>
+        </>
+       
     )
 }
